42103c25686c56f21d552d47a40a5f3c04cbb889
ipcrm <mcadorette@gmail.com>
2016-12-27 19:52:05 -0500

    set hiera-http version

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -34,7 +34,7 @@ RSpec.configure do |c|
 
       if ENV['BEAKER_custom_hiera'] == 'true'
         on host, puppet('module', 'install', 'crayfishx-hiera_http'), { :acceptable_exit_codes => [0,1] }
-        shell '/opt/puppetlabs/puppet/bin/gem install hiera-http', :acceptable_exit_codes => [0]
+        shell '/opt/puppetlabs/puppet/bin/gem install hiera-http -v 1.4.0', :acceptable_exit_codes => [0]
         scp_to(host, 'hiera.yaml', '/etc/puppetlabs/puppet/hiera.yaml')
       end
 
